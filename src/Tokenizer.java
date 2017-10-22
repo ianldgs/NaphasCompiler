02d65062ae05b415eecdd548e156053835c2cef9
Ian Luca <ian.naph@gmail.com>
2017-10-22 15:59:13 -0200

    WIP initial implementation of tokenizer

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,6 +1,7 @@
 public class Tokenizer {
     private enum State {
-        INITIAL
+        INITIAL,
+        IDENTIFIER,
     }
 
     private String code;
@@ -8,6 +9,8 @@ public class Tokenizer {
     private State state = State.INITIAL;
 
     private int position = 0;
+
+    private String lexeme;
 
     public Tokenizer(String code) {
         this.code = code;
@@ -17,12 +20,20 @@ public class Tokenizer {
         return code.charAt(position++);
     }
 
-    public Token getNextToken() {
-        switch (state) {
-            case INITIAL:
-                return Token.IDENTIFIER;
+    public Token getNextToken() throws Exception {
+        while (position < code.length()) {
+            char c = getNextCharacter();
+
+            switch (state) {
+                case INITIAL:
+                    if (c == '=') {
+                        return Token.OP_ATTRIB;
+                    }
+            }
+
+            throw new Exception("Invalid identifier: " + c);
         }
 
-        return Token.IDENTIFIER;
+        return null;
     }
 }
---
+++ tests/TokenizerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenizerTest {
+    @Test
+    void testConstAttrib() throws Exception {
+        Tokenizer tokenizer = new Tokenizer("const char a = 'b';");
+
+        assertEquals(Token.CREATE_CONST, tokenizer.getNextToken());
+        assertEquals(Token.TYPE_CHAR, tokenizer.getNextToken());
+        assertEquals(Token.IDENTIFIER, tokenizer.getNextToken());
+        assertEquals(Token.OP_ATTRIB, tokenizer.getNextToken());
+        assertEquals(Token.LIT_CHAR, tokenizer.getNextToken());
+        assertEquals(Token.TERMINATOR, tokenizer.getNextToken());
+    }
+}
