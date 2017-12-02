26808d2c5c31f8f88f525e0b92342cc0886303de
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-02 18:48:15 -0200

    ADD CaseStatement

---
+++ src/com/naphas/statements/CaseStatement.java
@@ -0,0 +1,28 @@
+package com.naphas.statements;
+
+import com.naphas.Parser;
+import com.naphas.Type;
+import com.naphas.exceptions.SyntaxException;
+import com.naphas.interfaces.Statement;
+
+public class CaseStatement implements Statement {
+    @Override
+    public void execute() throws SyntaxException {
+        Parser parser = Parser.getInstance();
+
+        parser.match(Type.CASE);
+
+        if(parser.isEqualReceivedToken(Type.LIT_STRING)) {
+            parser.match(Type.LIT_STRING);
+        }
+        else if(parser.isEqualReceivedToken(Type.LIT_CHAR)) {
+            parser.match(Type.LIT_CHAR);
+        }
+        else {
+            parser.match(Type.LIT_INT);
+        }
+
+        BlockStatement blockStatement = new BlockStatement();
+        blockStatement.execute();
+    }
+}
