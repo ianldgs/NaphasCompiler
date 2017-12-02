e0482b7a2f27614769d4cc1fa6b7dae980daa320
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-02 18:37:13 -0200

    ADD BlockStatement

---
+++ src/com/naphas/statements/BlockStatement.java
@@ -0,0 +1,22 @@
+package com.naphas.statements;
+
+import com.naphas.Parser;
+import com.naphas.Type;
+import com.naphas.exceptions.SyntaxException;
+import com.naphas.interfaces.Statement;
+
+public class BlockStatement implements Statement {
+    @Override
+    public void execute() throws SyntaxException {
+        Parser parser = Parser.getInstance();
+
+        parser.match(Type.START_BLOCK);
+
+        while(!parser.isEqualReceivedToken(Type.END_BLOCK)) {
+            ExpressionStatement expressionStatement = new ExpressionStatement();
+            expressionStatement.execute();
+        }
+
+        parser.match(Type.END_BLOCK);
+    }
+}
