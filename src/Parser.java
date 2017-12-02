e97aaf5e75a7de4cd7767445c95ff58ebde88505
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-02 16:50:33 -0200

    WIP added match method, that returns a boolean upon getting the expected token, and throws a SyntaxException if it doesn't.

--- src/Parser.java
+++ src/Parser.java
@@ -16,4 +16,26 @@ public class Parser {
     private void consume() {
         this.position++;
     }
+
+    public boolean match(Token expected) throws SyntaxException {
+        Token received = this.getNextToken();
+
+        boolean isEqualReceivedToken = received.equals(expected);
+        this.consume();
+
+        if (isEqualReceivedToken) {
+            return isEqualReceivedToken;
+        }
+        else {
+            String message = "Expected token %1, but got %2 instead.";
+
+            message = String.format(
+                    message,
+                    expected,
+                    received
+            );
+
+            throw new SyntaxException(message);
+        }
+    }
 }
