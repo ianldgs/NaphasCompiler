ebb8ad91fdd6147356de72ba43e0cf4e5b0274f9
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-02 18:36:47 -0200

    ADD placeholder ExpressionStatement

---
+++ src/com/naphas/statements/ExpressionStatement.java
@@ -0,0 +1,11 @@
+package com.naphas.statements;
+
+import com.naphas.exceptions.SyntaxException;
+import com.naphas.interfaces.Statement;
+
+public class ExpressionStatement implements Statement {
+    @Override
+    public void execute() throws SyntaxException {
+
+    }
+}
