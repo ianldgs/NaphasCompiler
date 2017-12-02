c0ed62787a599feb88c85a34a2a096acf346f654
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-02 18:38:41 -0200

    ADD IfStatement

---
+++ src/com/naphas/statements/IfStatement.java
@@ -0,0 +1,24 @@
+package com.naphas.statements;
+
+import com.naphas.Parser;
+import com.naphas.Type;
+import com.naphas.exceptions.SyntaxException;
+import com.naphas.interfaces.Statement;
+
+public class IfStatement implements Statement {
+    @Override
+    public void execute() throws SyntaxException {
+        Parser parser = Parser.getInstance();
+
+        parser.match(Type.IF);
+        parser.match(Type.START_EXP);
+
+        LogicalStatement logicalStatement = new LogicalStatement();
+        logicalStatement.execute();
+
+        parser.match(Type.END_BLOCK);
+
+        BlockStatement blockStatement = new BlockStatement();
+        blockStatement.execute();
+    }
+}
