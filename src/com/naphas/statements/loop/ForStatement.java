41ca0e7401dfd15038aee2afe2fc746866744945
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-02 22:31:04 -0200

    WIP ForStatement

---
+++ src/com/naphas/statements/loop/ForStatement.java
@@ -0,0 +1,10 @@
+package com.naphas.statements.loop;
+
+import com.naphas.statements.interfaces.Statement;
+
+public class ForStatement implements Statement {
+    @Override
+    public void execute() {
+
+    }
+}
