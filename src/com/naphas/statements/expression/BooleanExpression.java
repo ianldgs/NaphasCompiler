1720e90afe6fbb448630f9edd7d514aa406c3b62
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-03 00:20:38 -0200

    ADD BooleanExpression

---
+++ src/com/naphas/statements/expression/BooleanExpression.java
@@ -0,0 +1,22 @@
+package com.naphas.statements.expression;
+
+import com.naphas.exceptions.SyntaxException;
+import com.naphas.statements.interfaces.Statement;
+import com.naphas.statements.operators.LogicalStatement;
+import com.naphas.statements.operators.RelationalStatement;
+
+public class BooleanExpression extends ExpressionStatement {
+    @Override
+    public void execute() throws SyntaxException {
+        Statement statement;
+
+        if(LogicalStatement.isEqualStartToken()) {
+            statement = new LogicalStatement();
+        }
+        else {
+            statement = new RelationalStatement();
+        }
+
+        statement.execute();
+    }
+}
