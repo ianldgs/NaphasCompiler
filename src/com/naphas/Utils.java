6c0516295c3d504ef97cd31fff58601b709657de
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-03 20:47:06 -0200

    Refactored operator token check as a Util method

--- src/com/naphas/Utils.java
+++ src/com/naphas/Utils.java
@@ -1,6 +1,22 @@
 package com.naphas;
 
+import com.naphas.exceptions.SyntaxException;
+
+import java.util.ListIterator;
+
 public class Utils {
+
+    public static void validateTypesAllowed(ListIterator<Type> allowedTypes) throws SyntaxException {
+        Parser parser = Parser.getInstance();
+
+        while(allowedTypes.hasNext()) {
+            Type type = allowedTypes.next();
+
+            if(parser.isEqualReceivedToken(Type.OP_NOT)) {
+                parser.match(Type.OP_NOT);
+            }
+        }
+    }
 
     public static boolean isEqualStartToken(Type[] tokens) {
         Parser parser = Parser.getInstance();
--- src/com/naphas/statements/ValueStatement.java
+++ src/com/naphas/statements/ValueStatement.java
@@ -2,6 +2,7 @@ package com.naphas.statements;
 
 import com.naphas.Parser;
 import com.naphas.Type;
+import com.naphas.Utils;
 import com.naphas.exceptions.SyntaxException;
 import com.naphas.statements.interfaces.Statement;
 
@@ -23,13 +24,7 @@ public class ValueStatement implements Statement {
     public void execute() throws SyntaxException {
         Parser parser = Parser.getInstance();
 
-        while(this.allowedTypes.hasNext()) {
-            Type type = this.allowedTypes.next();
-
-            if(parser.isEqualReceivedToken(Type.OP_NOT)) {
-                parser.match(Type.OP_NOT);
-            }
-        }
+        Utils.validateTypesAllowed(this.allowedTypes);
 
         if(this.allowedNotIdentifier && parser.isEqualReceivedToken(Type.OP_NOT)) {
             parser.match(Type.OP_NOT);
