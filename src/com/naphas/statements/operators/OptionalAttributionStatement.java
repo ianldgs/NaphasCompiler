c88c9e210edf7d36f5fea96b0b32cb494731ef9b
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-03 02:07:27 -0200

    ADD OptionalAttributionStatement

---
+++ src/com/naphas/statements/operators/OptionalAttributionStatement.java
@@ -0,0 +1,19 @@
+package com.naphas.statements.operators;
+
+import com.naphas.Parser;
+import com.naphas.Type;
+import com.naphas.exceptions.SyntaxException;
+
+public class OptionalAttributionStatement extends AttributionStatement {
+    @Override
+    public void execute() throws SyntaxException {
+        super.leftValue();
+
+        Parser parser = Parser.getInstance();
+
+        if(parser.isEqualReceivedToken(Type.OP_ATTRIB)) {
+            super.symbols();
+            super.rightValue();
+        }
+    }
+}
