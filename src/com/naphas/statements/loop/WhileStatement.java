3a7744898d82c9ddf31e2f6b8e47d0af210f2079
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-03 02:20:21 -0200

    ADD WhileStatement

---
+++ src/com/naphas/statements/loop/WhileStatement.java
@@ -0,0 +1,28 @@
+package com.naphas.statements.loop;
+
+import com.naphas.Parser;
+import com.naphas.Type;
+import com.naphas.exceptions.SyntaxException;
+import com.naphas.statements.BlockStatement;
+
+public class WhileStatement extends LoopStatement {
+    @Override
+    public void execute() throws SyntaxException {
+        Parser parser = Parser.getInstance();
+
+        boolean isDoWhile = parser.isEqualReceivedToken(Type.DO);
+
+        if(isDoWhile) {
+            DoStatement doStatement = new DoStatement();
+            doStatement.execute();
+        }
+
+        parser.match(Type.WHILE);
+        super.execute();
+
+        if(!isDoWhile) {
+            BlockStatement blockStatement = new BlockStatement();
+            blockStatement.execute();
+        }
+    }
+}
