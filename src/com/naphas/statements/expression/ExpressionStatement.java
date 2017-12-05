709f89283c655641495e22f0e66f8af12d9185ce
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-04 23:54:16 -0200

    ADD Loop and Conditional expressions to ExpressionStatement

--- src/com/naphas/statements/expression/ExpressionStatement.java
+++ src/com/naphas/statements/expression/ExpressionStatement.java
@@ -1,11 +1,26 @@
 package com.naphas.statements.expression;
 
+import com.naphas.Parser;
 import com.naphas.exceptions.SyntaxException;
+import com.naphas.statements.BlockStatement;
 import com.naphas.statements.interfaces.Statement;
 
 public class ExpressionStatement implements Statement {
     @Override
     public void execute() throws SyntaxException {
+        Parser parser = Parser.getInstance();
 
+        while (!parser.isEndOfFile() || !BlockStatement.isEqualEndStatement()) {
+            Statement statement = null;
+
+            if(LoopExpression.isEqualStartStatement()) {
+                statement = new LoopExpression();
+            }
+            else if(ConditionalExpression.isEqualStartStatement()) {
+                statement = new ConditionalExpression();
+            }
+
+            statement.execute();
+        }
     }
 }
