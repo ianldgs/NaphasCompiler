8460efb0994e3ac884d65a914596b9f0915a6726
Matheus Elias R. Moreira <mattelias@live.com>
2017-12-04 23:38:11 -0200

    ADD LoopExpression

---
+++ src/com/naphas/statements/expression/LoopExpression.java
@@ -0,0 +1,32 @@
+package com.naphas.statements.expression;
+
+import com.naphas.exceptions.SyntaxException;
+import com.naphas.statements.interfaces.Statement;
+import com.naphas.statements.loop.ForStatement;
+import com.naphas.statements.loop.WhileStatement;
+
+public class LoopExpression implements Statement {
+
+    @Override
+    public void execute() throws SyntaxException {
+        Statement statement = null;
+
+        if(ForStatement.isEqualStartStatement()) {
+            statement = new ForStatement();
+        }
+        else if (WhileStatement.isEqualStartStatement()) {
+            statement = new WhileStatement();
+        }
+
+        if(this.isEqualStartStatement()) {
+            statement.execute();
+        }
+    }
+
+    public boolean isEqualStartStatement() {
+        return (
+                ForStatement.isEqualStartStatement() ||
+                WhileStatement.isEqualStartStatement()
+        );
+    }
+}
